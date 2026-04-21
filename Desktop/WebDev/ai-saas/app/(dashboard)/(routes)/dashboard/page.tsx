58161dee4bbea3cd9d005fd859a38b1166b3103f
AliMessam <waqarlizada999@gmail.com>
2026-04-21 14:19:07 +0500

    Authentication with CLERK

---
+++ Desktop/WebDev/ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <>
+    <p className="text-3xl text-green-500 ">Hello AI SAAS</p>
+    <Button variant="default" size="lg" className="w-20">Click me</Button>
+    </>
+  );
+}
+
+export default DashboardPage;
+
+// 13:22
---
+++ Desktop/WebDev/ai-saas/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <>
+        <p className="text-3xl text-green-500 ">Landing Page (Public)</p>
+        </>
+    );
+}
+
+export default LandingPage;
